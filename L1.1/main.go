ce95cbbeafb8982a8b1ab84170b0f0ddbd2b5eda
Tommych123 <pmiltsev@mail.ru>
2025-08-20 17:13:47 +0300

    L1.2+fmt

--- L1.1/main.go
+++ L1.1/main.go
@@ -3,29 +3,28 @@ package main
 import "fmt"
 
 // Дана структура Human
-type Human struct{
+type Human struct {
 	Name string
-	Age int
+	Age  int
 }
 
 // Дана структура Action
-type Action struct{
+type Action struct {
 	Human
 }
 
 // Метод Say для структуры Human
-func (H *Human)Say(){
+func (H *Human) Say() {
 	fmt.Println("I am Human")
 }
 
 // Метод Say для структуры Action
-func (A *Action)Say(){
+func (A *Action) Say() {
 	fmt.Println("I am Action")
 }
 
-
 func main() {
-	A:=Action{Human: Human{Name: "Вася", Age: 18}} // Создание переменной Action
-	A.Say() // Происходит вызов метода для Action (I am Action)
-	A.Human.Say() // Происходит вызов метода для Human (I am Human)
+	A := Action{Human: Human{Name: "Вася", Age: 18}} // Создание переменной Action
+	A.Say()                                          // Происходит вызов метода для Action (I am Action)
+	A.Human.Say()                                    // Происходит вызов метода для Human (I am Human)
 }
