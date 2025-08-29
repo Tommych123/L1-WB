7d00685d8cdba36fe55a4979f01a003a5a8f96cb
Tommych123 <pmiltsev@mail.ru>
2025-08-29 09:46:47 +0300

    L1.5 comments

--- L1.5/main.go
+++ L1.5/main.go
@@ -8,16 +8,20 @@ import (
 )
 
 func main() {
+	// Ввод N из аргументов командной строки при запуске программы
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <N>")
 		return
 	}
+	// Считывание N и проверка на валидность
 	N, err := strconv.Atoi(os.Args[1])
 	if err != nil || N < 0 {
 		fmt.Println("Invalid N")
 		return
 	}
+	// Создание канала
 	ch := make(chan int)
+	// Запуск горутины для записи в канал
 	go func() {
 		i := 0
 		for {
@@ -26,11 +30,14 @@ func main() {
 			time.Sleep(1 * time.Second)
 		}
 	}()
+	// Создание таймера который сработает через N секунд
 	timeout := time.After(time.Duration(N) * time.Second)
 	for {
 		select {
+		// Считываем из канала и выводим
 		case v := <-ch:
 			fmt.Println(v)
+		// Завершаем программу по истечению времени
 		case <-timeout:
 			fmt.Println("Time is over")
 			return
